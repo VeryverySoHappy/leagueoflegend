5ae005fc50b8f3a473aa1f730fcdf51d3ebfbdd3
VeryverySoHappy <frontEnd0815@gmail.com>
2023-05-26 16:55:03 +0900

    BtnTop 제거

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import "../src/scss/styles.css";
+import "./css/styles.css";
 import Header from "./layouts/Header";
 import Nav from "./layouts/Nav";
-import BtnTop from "./layouts/BtnTop";
 import Index from "./pages/main/Index";
 import Streaming from "./pages/Streaming";
 import { default as MyPage } from "./pages/myPage/Index";
@@ -26,7 +25,6 @@ function App() {
                 <Header style={"home"} />
                 <Index />
                 <Nav />
-                <BtnTop />
               </>
             }
           />
@@ -38,7 +36,6 @@ function App() {
                 <Header title={"스포츠"} />
                 <Sports />
                 <Nav />
-                <BtnTop />
               </>
             }
           />
@@ -50,7 +47,6 @@ function App() {
                 <Header title={"스트리밍"} />
                 <Streaming />
                 <Nav />
-                <BtnTop />
               </>
             }
           />
@@ -59,10 +55,9 @@ function App() {
             path="/community"
             element={
               <>
-                <Header title={"스트리밍"} />
+                <Header title={"커뮤니티"} />
                 <Community />
                 <Nav />
-                <BtnTop />
               </>
             }
           />
@@ -74,7 +69,6 @@ function App() {
                 <Header title={"마이페이지"} />
                 <MyPage />
                 <Nav />
-                <BtnTop />
               </>
             }
           />
@@ -86,7 +80,6 @@ function App() {
                 <Header title={"회원가입"} icon />
                 <Join />
                 <Nav />
-                <BtnTop />
               </>
             }
           />
