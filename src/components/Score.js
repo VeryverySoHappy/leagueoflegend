1ff6c812ecbd151160a3ccb5ecd2d90cdbaa3fe7
VeryverySoHappy <frontEnd0815@gmail.com>
2023-05-26 16:56:01 +0900

    chart.js

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ['WIN', 'LOSE'],
+  datasets: [
+    {
+      data: [11, 7],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.8)',
+        'rgba(54, 162, 235, 0.8)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    }
+  }
+}
 
 function Score() {
   return (
-    <div>Score</div>
+    <Doughnut data={data} options={options}/>
   )
 }
 
